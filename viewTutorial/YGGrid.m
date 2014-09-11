3eb7d57ff7fef391b0027b16b0d001c77ef99552
Yaxi Gao <yaxigao@Yaxis-MacBook-Pro.local>
2014-09-10 22:34:30 -0700

    viewTutorial :)

---
+++ viewTutorial/YGGrid.m
@@ -0,0 +1,31 @@
+//
+//  YGGrid.m
+//  viewTutorial
+//
+//  Created by Yaxi Gao on 9/10/14.
+//  Copyright (c) 2014 Yaxi Gao. All rights reserved.
+//
+
+#import "YGGrid.h"
+
+@implementation YGGrid
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
